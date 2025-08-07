b7ba601ace14ae3ba9929e0861cdfa210c2d96b6
elijahgott <gottelijah@gmail.com>
2025-08-07 13:31:21 -0500

    refactored code for blogList - exercise 4.2

---
+++ part4/blogList/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('Error connecting to MongoDB:', error.message)
+    })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter) // error here
+
+module.exports = app
