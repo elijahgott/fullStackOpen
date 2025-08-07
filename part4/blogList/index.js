2159ec0afead1fb557e5b6e2a1620751755caa7d
elijahgott <gottelijah@gmail.com>
2025-08-07 13:08:00 -0500

    added part4

---
+++ part4/blogList/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = "mongodb+srv://elijah:Tree49house@cluster0.1h2zd.mongodb.net/blogList?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
