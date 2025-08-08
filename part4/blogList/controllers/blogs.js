b3d4041c9a559d71c21f161b27b7f6f84b8e79f4
elijahgott <gottelijah@gmail.com>
2025-08-08 15:39:41 -0500

    completed exercises for part 4b, skipped 4.11 and 4.12

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -18,4 +18,35 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
+// 4.13
+blogsRouter.delete('/:id', async (request, response) => {
+  try{
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  }
+  catch(exception){
+    response.status(404).end()
+  }
+})
+
+// 4.14
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+
+  const blog = {
+    title,
+    author,
+    url,
+    likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true, runValidators: true})
+  
+  if(!updatedBlog){
+    return response.status(404).send({ error: 'Blog does not exist'})
+  }
+
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -1,21 +1,136 @@
-const { test, after } = require('node:test')
+const { test, after, describe, beforeEach } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
+
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('With blogs in DB', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        
+        const blogObjs = helper.initialBlogs
+            .map(blog => new Blog(blog))
+        const promiseArray = blogObjs.map(blog => blog.save())
+        await Promise.all(promiseArray)
+    })
+
+    // 4.8
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    // 4.9
+    test('blogs have unique id\'s', async () => {
+        const response = await api.get('/api/blogs')
+        const ids = response.body.map(blog => blog.id)
+        const idSet = new Set()
+
+        for(const id of ids){
+            assert(!idSet.has(id))
+            idSet.add(id)
+        }
+    })
+
+    // 4.11
+    // if likes missing, default value to 0
 })
 
-test('blogs have unique id\'s', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
+describe('adding new blog', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        
+        const blogObjs = helper.initialBlogs
+            .map(blog => new Blog(blog))
+        const promiseArray = blogObjs.map(blog => blog.save())
+        await Promise.all(promiseArray)
+    })
+
+    // 4.10
+    test('POST successfully creates blog', async () => {
+        const newBlog = {
+            title: 'Joe Rogan missing link between humans and apes',
+            author: 'Me',
+            url: 'https://www.earth.com/news/missing-link-apes-early-humans/',
+            likes: 10
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        const blogs = response.body.map(blog => blog.title)
+        
+        assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+
+        assert(blogs.includes('Joe Rogan missing link between humans and apes'))
+    })
+
+    // 4.12
+    // if title or url values are missing, server responds with status code 400 Bad Request
+})
+
+describe('deleting blog', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        
+        const blogObjs = helper.initialBlogs
+            .map(blog => new Blog(blog))
+        const promiseArray = blogObjs.map(blog => blog.save())
+        await Promise.all(promiseArray)
+    })
+
+    // 4.13
+    test('delete blog from id', async () => {
+        const response = await api.get('/api/blogs')
+        const blogToDelete = response.body[0]
+
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+        
+        const responseAfterDeletion = await api.get('/api/blogs')
+
+        assert.strictEqual(responseAfterDeletion.body.length, response.body.length - 1)
+    })
+})
+
+describe('updating blog', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        
+        const blogObjs = helper.initialBlogs
+            .map(blog => new Blog(blog))
+        const promiseArray = blogObjs.map(blog => blog.save())
+        await Promise.all(promiseArray)
+    })
+
+    // 4.13
+    test('update likes from id', async () => {
+        const response = await api.get('/api/blogs')
+        const blogToUpdate = response.body[0]
+        blogToUpdate.likes = 333
+
+        await api
+            .put(`/api/blogs/${blogToUpdate.id}`)
+            .send(blogToUpdate)
+            .expect(200)
+        
+        const responseAfterUpdate = await api.get('/api/blogs')
+
+        assert.strictEqual(responseAfterUpdate.body[0].likes, 333)
+    })
 })
 
 after(async () => {
