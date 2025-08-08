7f90fc87f16a0a9b819fc2b89918bb3755055373
elijahgott <gottelijah@gmail.com>
2025-08-08 10:55:50 -0500

    completed exercise 4.8

---
+++ part4/blogList/tests/blog_api.test.js
@@ -0,0 +1,23 @@
+const { test, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('blogs have unique id\'s', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
