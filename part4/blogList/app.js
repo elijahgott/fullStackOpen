575bbeb9230d13f0878ec481520df39cc2c41869
elijahgott <gottelijah@gmail.com>
2025-08-11 18:28:16 -0500

    added user schema and router, from part 4c

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -19,6 +21,10 @@ mongoose
 
 app.use(express.json())
 
-app.use('/api/blogs', blogsRouter) // error here
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
