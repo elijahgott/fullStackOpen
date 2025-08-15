d747272280c51b60f55817b09b620f79f246c22c
elijahgott <gottelijah@gmail.com>
2025-08-15 12:40:13 -0500

    fix: frontend updates when signing in and signing out without refreshing

--- part5/blogListFrontend/src/App.jsx
+++ part5/blogListFrontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './components/Login'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 import CreateBlog from './components/CreateBlog'
 
 const App = () => {
@@ -32,6 +33,39 @@ const App = () => {
         }
     }
 
+    // for logging in
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+        console.log('logging in with', username, password)
+
+        try{
+            const user = await loginService.login({ username, password })
+            window.localStorage.setItem('loggedInUser', JSON.stringify(user))
+
+            setUser(user)
+            setUsername('')
+            setPassword('')
+
+            setMessage(`Signed in as ${username}`)
+            setNotificationClass('success')
+            setTimeout(() => {
+                setMessage(null)
+                setNotificationClass(null)
+            }, 3000)
+        }
+        catch(exception){
+            setMessage('Wrong username or password.')
+            setNotificationClass('error')
+            setTimeout(() => {
+                setMessage(null)
+                setNotificationClass(null)
+            }, 3000)
+        }
+    }
+
     useEffect(() => {
         blogService.getAll().then(blogs =>
             setBlogs( blogs )
@@ -51,14 +85,15 @@ const App = () => {
     return (
         <div>
             <Notification className={notificationClass} message={message} />
-            { user === null ?
-                <Login setNotificationClass={setNotificationClass} setMessage={setMessage} />
+            { user === null
+                ? <Login handleLogin={handleLogin} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />
                 : <>
                     <h2>Blogs</h2>
                     <div>
                         Signed in as {user.username}
                         <button onClick={() => {
                             window.localStorage.removeItem('loggedInUser')
+                            setUser(null)
                         }}>Log Out</button>
                     </div>
 
--- part5/blogListFrontend/src/components/Login.jsx
+++ part5/blogListFrontend/src/components/Login.jsx
@@ -1,49 +1,6 @@
-import { useState } from 'react'
-import loginService from '../services/login'
-import PropTypes from 'prop-types'
-
-const Login = ({ setNotificationClass, setMessage }) => {
-    const loginStyle = {
-        textAlign: 'center',
-        width: 'fit-content',
-        margin: '0 auto',
-    }
-
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [user, setUser] = useState(null)
-
-    const handleLogin = async (event) => {
-        event.preventDefault()
-        console.log('logging in with', username, password)
-
-        try{
-            const user = await loginService.login({ username, password })
-            window.localStorage.setItem('loggedInUser', JSON.stringify(user))
-
-            setUser(user)
-            setUsername('')
-            setPassword('')
-
-            setMessage(`Signed in as ${username}`)
-            setNotificationClass('success')
-            setTimeout(() => {
-                setMessage(null)
-                setNotificationClass(null)
-            }, 3000)
-        }
-        catch(exception){
-            setMessage('Wrong username or password.')
-            setNotificationClass('error')
-            setTimeout(() => {
-                setMessage(null)
-                setNotificationClass(null)
-            }, 3000)
-        }
-    }
-
+const Login = ({ handleLogin, username, setUsername, password, setPassword }) => {
     return (
-        <div style={loginStyle}>
+        <div>
             <h1>Login</h1>
             <form onSubmit={handleLogin}>
                 <input type="text" data-testid='username' value={username} name="Username" onChange={({ target }) => setUsername(target.value)} placeholder="Enter Username:" />
@@ -52,11 +9,6 @@ const Login = ({ setNotificationClass, setMessage }) => {
             </form>
         </div>
     )
-}
-
-Login.PropTypes = {
-    setNotificationClass: PropTypes.string.isRequired,
-    setMessage: PropTypes.string.isRequired
 }
 
 export default Login
