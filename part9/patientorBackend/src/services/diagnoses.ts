524b9af2947c37ee8aaebdfbf8382244ebbd104b
elijahgott <gottelijah@gmail.com>
2025-10-25 11:15:52 -0500

    9.8-9.11: completed first half of exercises for part 9c

---
+++ part9/patientorBackend/src/services/diagnoses.ts
@@ -0,0 +1,14 @@
+import diagnosisData from '../../data/diagnoses';
+import { Diagnosis } from './types';
+
+const diagnoses : Diagnosis[] = diagnosisData;
+
+const getEntries = () : Diagnosis[] => {
+  return diagnoses;
+};
+
+const addDiagnosis = () => {
+  return null;
+};
+
+export default { getEntries, addDiagnosis };
